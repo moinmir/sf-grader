21176a254f0afb908c4672c170d011e7576893f0
Amit Aryeh Levy <amit@amitlevy.com>
2021-12-27 19:07:38 -0800

    Update snapfaas version

--- shell.nix
+++ shell.nix
@@ -5,8 +5,8 @@ let
   snapfaas = import (fetchFromGitHub {
     owner = "princeton-sns";
     repo = "snapfaas";
-    rev = "1d634cb66d69c4c5eed741d19ff6ff202dbd66fa";
-    sha256 = "1vrmm5nmydyhf3xbri9mpl9kpcd4ybb1waaynkx99l1hz9md4rg4";
+    rev = "d9da995d2b72ccfc5fe18500c2e71d8a1dff7c21";
+    sha256 = "08lsw6sslfvpy461cgdas3hqk226yzhyffr4q87cgc2w940vnxc4";
   }) { inherit pkgs; release = false; };
 in mkShell {
   buildInputs = [ lkl snapfaas ];
