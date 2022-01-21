21a7b246ab7e99e412298755e6e9cba3d0c44ca3
Amit Aryeh Levy <amit@amitlevy.com>
2022-01-21 14:17:43 -0800

    parameterize nix shell script

--- shell.nix
+++ shell.nix
@@ -1,13 +1,19 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import <nixpkgs> {},
+  snapfaasSha256 ? "19c6r7h7zrww3n9wjyznzjhq4avjyixzc846mvavr5x7nqwa1qva",
+  snapfaasRev ? "8c84242b6b682f9b3bd0cb2a075a7a8c3dbfb81d",
+  snapfaasSrc ? pkgs.fetchFromGitHub {
+    owner = "princeton-sns";
+    repo = "snapfaas";
+    rev = snapfaasRev;
+    sha256 = snapfaasSha256;
+  },
+  release ? false
+}:
 
 with pkgs;
 let
-  snapfaas = import (fetchFromGitHub {
-    owner = "princeton-sns";
-    repo = "snapfaas";
-    rev = "d9da995d2b72ccfc5fe18500c2e71d8a1dff7c21";
-    sha256 = "08lsw6sslfvpy461cgdas3hqk226yzhyffr4q87cgc2w940vnxc4";
-  }) { inherit pkgs; release = false; };
+  snapfaas = import snapfaasSrc { inherit pkgs release; };
 in mkShell {
   buildInputs = [ lkl snapfaas ];
   shellHook = ''
