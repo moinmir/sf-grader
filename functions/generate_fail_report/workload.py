9a7759fa96aeb6a596c5ccadd1da040dc875b39f
Moin <moin@princeton.edu>
2022-04-29 15:50:48 -0400

    marina sucks

--- functions/generate_fail_report/workload.py
+++ functions/generate_fail_report/workload.py
@@ -42,10 +42,12 @@ def app_handle(args, context, syscall):
     err = str(test_lines[0]['error']['compile'])
     
     err = err[ err.find("/") : ]
-    err = err[ err.find("/") : ]
-    err = err[ err.find("/") : ]
-    
     print(err)
+    err = err[ err.find("/") : ]
+    print(err)
+    err = err[ err.find("/") : ]
+    print(err)
+
     print("here 2")
 
     
