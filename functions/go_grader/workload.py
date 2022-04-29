7afb478666e63509d7abc90c4281a594ec08dfba
Moin <moin@princeton.edu>
2022-04-29 14:12:55 -0400

    marina sucks

--- functions/go_grader/workload.py
+++ functions/go_grader/workload.py
@@ -22,7 +22,7 @@ def handle(req, syscall):
     return result
 
 def app_handle(args, state, syscall):
-    print("******************************HERE")
+    print("**************************************************************************************************************************************************HERE")
     os.system("ifconfig lo up")
     # Fetch and untar submission tarball
     assignment = state["metadata"]["assignment"]
