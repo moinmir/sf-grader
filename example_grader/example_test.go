47781a28ff05a5578445b38864204ec83f9cc423
Moin <moin@princeton.edu>
2022-04-30 19:56:55 -0400

    marina is amazing

--- example_grader/example_test.go
+++ example_grader/example_test.go
@@ -5,14 +5,14 @@ import (
 	"testing"
 )
 
-// func TestNegate(t *testing.T) {
-// 	if example.Negate(true) {
-// 		t.Errorf("Expected false, was true")
-// 	}
-// }
-
 func TestCorrect(t *testing.T) {
 	if example.NegateCorrect(true) {
+		t.Errorf("Expected false, was true")
+	}
+}
+
+func TestNegate(t *testing.T) {
+	if example.Negate(true) {
 		t.Errorf("Expected false, was true")
 	}
 }
--- functions/go_grader/workload.py
+++ functions/go_grader/workload.py
@@ -47,7 +47,7 @@ def app_handle(args, state, syscall):
             with tempfile.NamedTemporaryFile(suffix=".tar.gz") as script_tar:
                 script_tar_data = syscall.read_key(
                     bytes("cos316/%s/grading_script" % assignment, "utf-8"))
-                    
+
                 script_tar.write(script_tar_data)
                 script_tar.flush()
                 with tempfile.TemporaryDirectory() as script_dir:
