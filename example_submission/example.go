9071c3d9de45ccbe0cf4dc7c7c0e4fc25675db56
Moin <moin@princeton.edu>
2022-04-30 12:19:27 -0400

    marina is amazing

--- example_submission/example.go
+++ example_submission/example.go
@@ -24,6 +24,13 @@ package example
 // cannot use !x (type bool) as type int in return argument
 func Negate(x bool) bool {
 
-	panic("hello")
+	// panic("hello")
 	return !x
 }
+
+GENERATE REPORT
+Output:
+['Submitted 01/21/22 20:56:47 +0000\n', '## Grade: 0.00%', '  * 0 points of a possible 10', '  * Passed   0 / 0  tests     (0 failed)', '    * Passed  0 / 0 subtests  (0 failed)', '## Correctness Tests']
+
+
+FINISHED RUNNING
