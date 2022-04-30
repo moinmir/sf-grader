2efefb269088292cf4cb043e42fdbd9bf58e0515
Moin <moin@princeton.edu>
2022-04-30 18:38:30 -0400

    marina is amazing

--- functions/generate_report/workload.py
+++ functions/generate_report/workload.py
@@ -28,7 +28,13 @@ def app_handle(args, context, syscall):
 
     print("\n\ngrade[\"tests\"]")
     print(grade["tests"])
-    
+
+    broken_tests = []
+    for i, test in enumerate(grade["tests"]):
+        if test["action"] == "run":
+            if i + 1 < len(grade["tests"]) and grade["tests"][i + 1]["action"] == "run":
+                broken_tests.append(test["test"])
+
     grade["tests"] = [test for test in grade["tests"] if test["action"] in ["pass", "fail"]]
     correctness_tests = [ test for test in grade["tests"] if not ("performance" in test["conf"] and test["conf"]["performance"])]
     performance_tests = [ test for test in grade["tests"] if ("performance" in test["conf"] and test["conf"]["performance"]) ]
