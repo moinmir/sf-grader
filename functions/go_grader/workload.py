848022aaf0fe3529b71d48930f452ab2538b1889
Moin <moin@princeton.edu>
2022-04-30 12:45:41 -0400

    marina is amazing

--- functions/go_grader/workload.py
+++ functions/go_grader/workload.py
@@ -3,17 +3,18 @@ import tempfile
 import os
 import subprocess
 
+
 def handle(req, syscall):
     args = req["args"]
     context = req["context"]
     workflow = req["workflow"]
     result = app_handle(args, context, syscall)
-    
+
     if "error" in result and "compile" in result["error"]:
-        workflow =  req["workflowfail"]
+        workflow = req["workflowfail"]
     else:
         workflow = req["workflow"]
-        
+
     if len(workflow) > 0:
         next_function = workflow.pop(0)
         print("\n\nNext function: %s" % next_function)
@@ -25,6 +26,7 @@ def handle(req, syscall):
         }))
     return result
 
+
 def app_handle(args, state, syscall):
     print("\n\n\n\n========================================")
     print("GO GRADER")
@@ -32,76 +34,86 @@ def app_handle(args, state, syscall):
     # Fetch and untar submission tarball
     assignment = state["metadata"]["assignment"]
     with tempfile.NamedTemporaryFile(suffix=".tar.gz") as submission_tar:
-        submission_tar_data = syscall.read_key(bytes(args["submission"], "utf-8"))
+        submission_tar_data = syscall.read_key(
+            bytes(args["submission"], "utf-8"))
         submission_tar.write(submission_tar_data)
         submission_tar.flush()
         with tempfile.TemporaryDirectory() as submission_dir:
             os.system("mkdir -p %s" % submission_dir)
-            os.system("tar -C %s -xzf %s --strip-components=1" % (submission_dir, submission_tar.name))
+            os.system("tar -C %s -xzf %s --strip-components=1" %
+                      (submission_dir, submission_tar.name))
 
             # Fetch and untar grading script tarball
             with tempfile.NamedTemporaryFile(suffix=".tar.gz") as script_tar:
-                script_tar_data = syscall.read_key(bytes("cos316/%s/grading_script" % assignment, "utf-8"))
+                script_tar_data = syscall.read_key(
+                    bytes("cos316/%s/grading_script" % assignment, "utf-8"))
                 script_tar.write(script_tar_data)
                 script_tar.flush()
                 with tempfile.TemporaryDirectory() as script_dir:
-                    os.system("tar -C %s -xzf %s" % (script_dir, script_tar.name))
+                    os.system("tar -C %s -xzf %s" %
+                              (script_dir, script_tar.name))
 
                     # OK, run tests
                     os.putenv("GOCACHE", "%s/.cache" % script_dir)
-                    os.putenv("GOROOT", "/srv/usr/lib/go") 
+                    os.putenv("GOROOT", "/srv/usr/lib/go")
                     os.putenv("SOLUTION_DIR", submission_dir)
-                    os.putenv("PATH", "%s:%s" % ("/srv/usr/lib/go/bin", os.getenv("PATH")))
+                    os.putenv("PATH", "%s:%s" %
+                              ("/srv/usr/lib/go/bin", os.getenv("PATH")))
                     os.chdir(script_dir)
                     if os.path.exists("pretest") and os.access("pretest", os.X_OK):
                         os.system("./pretest")
                     compiledtest = subprocess.Popen("go test -c -o /tmp/grader", shell=True,
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     compileout, compileerr = compiledtest.communicate()
 
                     final_results = []
 
                     if compiledtest.returncode != 0:
                         print("COMPILATION FAILED\n\n")
-                        out = { "error": { "compile": str(compileerr), "returncode": compiledtest.returncode } }
+                        out = {"error": {"compile": str(
+                            compileerr), "returncode": compiledtest.returncode}}
                         final_results.append(json.dumps(out))
                         print(final_results)
-                        key = os.path.join(os.path.splitext(args["submission"])[0], "test_results.jsonl")
-                        syscall.write_key(bytes(key, "utf-8"), bytes('\n'.join(final_results), "utf-8"))
+                        key = os.path.join(os.path.splitext(args["submission"])[
+                                           0], "test_results.jsonl")
+                        syscall.write_key(
+                            bytes(key, "utf-8"), bytes('\n'.join(final_results), "utf-8"))
 
                         return out
                     print("COMPILATION SUCCEEDED\n\n")
-                    
-                    testrun = subprocess.Popen("/tmp/grader -test.v | /srv/usr/lib/go/pkg/tool/linux_amd64/test2json", shell=True,
-                            stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
 
+                    testrun = subprocess.Popen("/tmp/grader -test.v | /srv/usr/lib/go/pkg/tool/linux_amd64/test2json",
+                                               shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
 
-                    if testrun.stderr:
-                    for test_result in testrun.stderr:
-                        print("hi ya")
-                        print(test_result)
+                    if testrun.stderr is not None:
+                        for test_result in testrun.stderr:
+                            print("hi ya")
+                            print(test_result)
 
                     for test_result in testrun.stdout:
                         tr = json.loads(test_result)
                         print(tr)
 
                         # if "pass" not in tr or "fail" not in tr:
-                        #    final_results.append(error message)  
+                        #    final_results.append(error message)
 
                         if tr["Action"] in ["pass", "fail", "run"]:
-                            tr = dict((name.lower(), val) for name, val in tr.items())
+                            tr = dict((name.lower(), val)
+                                      for name, val in tr.items())
                             final_results.append(json.dumps(tr))
                     print(final_results)
-                    key = os.path.join(os.path.splitext(args["submission"])[0], "test_results.jsonl")
-                    syscall.write_key(bytes(key, "utf-8"), bytes('\n'.join(final_results), "utf-8"))
+                    key = os.path.join(os.path.splitext(args["submission"])[
+                                       0], "test_results.jsonl")
+                    syscall.write_key(bytes(key, "utf-8"),
+                                      bytes('\n'.join(final_results), "utf-8"))
                     testrun.wait()
                     if testrun.returncode >= 0:
-                        return { "test_results": key }
+                        return {"test_results": key}
                     else:
                         _, errlog = testrun.communicate()
                         print("Error log:")
                         print(errlog)
-                        return { "error": { "testrun": str(errlog), "returncode": testrun.returncode } }
+                        return {"error": {"testrun": str(errlog), "returncode": testrun.returncode}}
     return {}
 
 
@@ -116,8 +128,6 @@ def app_handle(args, state, syscall):
 # {'Action': 'pass', 'Test': 'TestNegate'}
 # {'Action': 'output', 'Output': 'PASS\n'}
 # {'Action': 'pass'}
-
-
 
 
 # {'Action': 'run', 'Test': 'TestNegate'}
