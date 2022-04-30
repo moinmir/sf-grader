230eec2d075815ae4eda6b5633c46dfec981a536
Moin <moin@princeton.edu>
2022-04-30 12:29:39 -0400

    marina is amazing

--- example_submission/example.go
+++ example_submission/example.go
@@ -3,9 +3,9 @@ package example
 //==================================================
 // This compiles
 //==================================================
-func Negate(x bool) bool {
-	return !x
-}
+// func Negate(x bool) bool {
+// 	return !x
+// }
 
 //==================================================
 // This doesn't compile
@@ -21,23 +21,26 @@ func Negate(x bool) bool {
 // 	return !x
 // }
 
-// cannot use !x (type bool) as type int in return argument
-// func Negate(x bool) bool {
+//==================================================
+// This panics
+//==================================================
 
-// 	panic("hello")
-// 	return !x
-// }
+// cannot use !x (type bool) as type int in return argument
+func Negate(x bool) bool {
+	panic("hello")
+	return !x
+}
 
 // Output:
-// ['Submitted 01/21/22 20:56:47 +0000\n', '## Grade: 0.00%', 
-// '  * 0 points of a possible 10', 
-// '  * Passed   0 / 0  tests     (0 failed)', 
-// '    * // Passed  0 / 0 subtests  (0 failed)', 
+// ['Submitted 01/21/22 20:56:47 +0000\n', '## Grade: 0.00%',
+// '  * 0 points of a possible 10',
+// '  * Passed   0 / 0  tests     (0 failed)',
+// '    * // Passed  0 / 0 subtests  (0 failed)',
 // '## Correctness Tests']
 
 // Output:
-// ['Submitted 01/21/22 20:56:47 +0000\n', '## Grade: 100.00%', 
+// ['Submitted 01/21/22 20:56:47 +0000\n', '## Grade: 100.00%',
 // '  * 10 points of a possible 10',
-//  '  * Passed   1 / 1  tests     (0 failed)', 
+//  '  * Passed   1 / 1  tests     (0 failed)',
 // '    * Passed  0 / 0 subtests  (0 failed)',
 //  '## Correctness Tests', '### 1. Negate', '                               -- test passed --']
