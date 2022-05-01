8ba1c6458157c27b7cf739705fedae88a030522b
Moin <moin@princeton.edu>
2022-04-30 22:37:14 -0400

    marina is amazing

--- example_submission/example.go
+++ example_submission/example.go
@@ -1,8 +1,11 @@
 package example
 
+import "fmt"
+
 //==================================================
 // This compiles
 //==================================================
 func NegateCorrect(x bool) string {
+	x := 5 / 0
 	return !x
 }
